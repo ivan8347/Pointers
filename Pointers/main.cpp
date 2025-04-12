c528ecddf3b9fefdcc48340c3649c65d398caf03
kit <kit@DESKTOP-EHKDJE1>
2025-04-12 13:01:40 +0300

    Add project files.

---
+++ Pointers/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello Pointers" << endl;
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;
+	cout << &a << endl;
+	cout << pa << endl;
+	cout << *pa << endl;
+
+}
