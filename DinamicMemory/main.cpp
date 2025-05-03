c0d3f5f8e6be15bfb0e89f85b34f88f8edb8cced
kit <kit@DESKTOP-EHKDJE1>
2025-05-03 12:07:09 +0300

    AddElement

--- DinamicMemory/main.cpp
+++ DinamicMemory/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
-
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
 
 void main()
 {
@@ -8,18 +9,42 @@ void main()
 	int n;
 	cout << "¬ведите размер массива: "; cin >> n;
 	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	int value;
+	cout << "¬ведите добавленное значение: "; cin >> value;
+	//буферный массив нужного размера
+	int* buffer = new int[n = 1];
+	//копируем всЄ содержимое исходного в буфер
+	for (int i = 0; i < n; i++)
+	{
+		buffer [i] = arr[i];
+	}
+	delete[] arr;
+	arr[n] = buffer;
+	//только после этго в массиве по€вл€етс€ элимент 
+	arr[n] = value;
+	//как в массив добавилс€ элимент кол-во элементов увеличиваетс€ на один
+	n++;
+	Print(arr, n);
+
+	delete[] arr;
+
+
+
+}
+void FillRand(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
-
 	}
+}
+void Print(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
-	delete[] arr;
-
-
-
 }
--- Split/main.cpp
+++ Split/main.cpp
@@ -9,25 +9,32 @@ void main()
 	int arr[n];
 	FILLRand(arr, n);
 	Print(arr, n);
-
+	cout << "\n";
+	//считаем количество чётных и нечётных элементов
 	int even_count = 0;
 	int odd_count = 0;
 	for (int i = 0; i < n; i++)
 	{
-		if (arr[i] % 2 == 0)even_count++;
-		else odd_count++;
+		// arr[i] % 2 == 0 ? even_count++ : odd_count++;
+		//if (arr[i] % 2 == 0)  even_count++ ;
+		//else odd_arr[k++]=arr[i];
+		( arr[i] % 2 == 0 ? even_count : odd_count)++;
+
 	}
 	//выделяем память для чётных и не чётных значений
 	int* even_arr = new int[even_count];
 	int* odd_arr = new int[odd_count];
-	//копируем количество
+	//копируем чётные и нечётные значения в массивы
 	for (int i = 0,j=0,k=0; i < n; i++)
 	{
-		if (arr[i] % 2 == 0)even_arr[j++] = arr[i];
-		else odd_arr[k++] = arr[i];
+		//if (arr[i] % 2 == 0)even_arr[j++] = arr[i];
+		//else odd_arr[k++] = arr[i];
+		( arr[i] % 2 == 0 ? even_arr[j++] : odd_arr[k++])=arr[i];
 	}
+	//выводим результат
 	Print(even_arr, even_count);
 	Print(odd_arr, odd_count);
+	//удаляем память
 	delete[] odd_arr;
 	delete[] even_arr;
 
